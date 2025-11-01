ee31eee49054f76910fae0004ac6f3e352f66388
Luan Clemente Costa <79475114+LuanClemente@users.noreply.github.com>
2025-11-01 17:40:50 +0000

    Salve para mim

--- 01.11.25/aula1.py
+++ 01.11.25/aula1.py
@@ -1,154 +1,169 @@
-# nome = input("Digite seu nome: ") #Dando valor a variavel nome
-# idade = int(input("Digite sua idade: ")) #Dando valor a variavel idade
+# # nome = input("Digite seu nome: ") #Dando valor a variavel nome
+# # idade = int(input("Digite sua idade: ")) #Dando valor a variavel idade
 
-# print(f"Olá, {nome}! Você tem {idade} anos de idade") #Utilizando o "F" de format
-# print(nome) #printando apenas a variavel nome
-# print(idade) #printando apenas a variavel idade
+# # print(f"Olá, {nome}! Você tem {idade} anos de idade") #Utilizando o "F" de format
+# # print(nome) #printando apenas a variavel nome
+# # print(idade) #printando apenas a variavel idade
+
+# # #=======================================================================================================================================#
+# # print("============Sem o 'F' do format===============")
+# # print("Nome: ",nome, "\nIdade: ",idade, "\n") #\n é para quebra de linha
+# # #=======================================================================================================================================#
+# # print("============Usando metodo format()============\n")
+# # print("Nome: {}, Idade: {}\n".format(nome,idade))
+# # #=======================================================================================================================================#
+# # print("============Usando % na formatação============\n")
+# # print("Nome: %s, Idade: %d"%(nome,idade))
+# # # d = decimal inteiro s = string
+# # mensagem = "Está é uma linha.\nE esta é outra linha."
+
+# # print(mensagem)
+
+# # mensagem_multilinha="""Esta é minha primeira linha.
+# # Está é minha segunda linha."""
+
+# # print(mensagem_multilinha)
+
+# # #======================================================================================================================================#
+
+# # print("======Exercico de uso do AND, OR e NOT======\n")
+
+# # n1 = True
+# # n2 = False
+
+# # #operador and
+# # res1 = n1 and n2
+# # print("And: ", res1)
+
+# # #Operador OR
+# # res2 = n1 or n2
+# # print ("OR: ", res2)
+
+# # #Operador NOT
+
+# # res3 = not n1
+# # print("NOT: ", res3)
 
 # #=======================================================================================================================================#
-# print("============Sem o 'F' do format===============")
-# print("Nome: ",nome, "\nIdade: ",idade, "\n") #\n é para quebra de linha
+
+# # print("===========Operadores Lógicos===========\n")
+
+# # idade = 32
+# # altura = 1.75
+
+# # resultado = (idade >= 18) and (altura >= 1.70)
+# # msg = "Pode participar do evento? " + str(resultado)
+# # print(msg)
+
 # #=======================================================================================================================================#
-# print("============Usando metodo format()============\n")
-# print("Nome: {}, Idade: {}\n".format(nome,idade))
+
+# # print("==========Programa de disparo de alarme==========\n")
+
+# # porta = "a"
+# # janela = "f"
+
+# # alarme = (porta == "f") and (janela == "a")
+
+# # mgs = "Alarme disparado? " + str(alarme)
+# # print(mgs)
+
 # #=======================================================================================================================================#
-# print("============Usando % na formatação============\n")
-# print("Nome: %s, Idade: %d"%(nome,idade))
-# # d = decimal inteiro s = string
-# mensagem = "Está é uma linha.\nE esta é outra linha."
 
-# print(mensagem)
+# # print ("=Adição, subtração, multiplicação, divisão, resto da divisão e exponenciação=\n")
 
-# mensagem_multilinha="""Esta é minha primeira linha.
-# Está é minha segunda linha."""
+# # n1 = 10
+# # n2 = 3
 
-# print(mensagem_multilinha)
+# # #adição
+# # soma = n1 + n2
+# # print("Adição: ", soma)
 
-# #======================================================================================================================================#
+# # #subtração
 
-# print("======Exercico de uso do AND, OR e NOT======\n")
+# # sub = n1 - n2
+# # print("Subtração: ", sub)
 
-# n1 = True
-# n2 = False
+# # #multiplicação
 
-# #operador and
-# res1 = n1 and n2
-# print("And: ", res1)
+# # mult = n1 * n2
+# # print("Multiplicação: ", mult)
 
-# #Operador OR
-# res2 = n1 or n2
-# print ("OR: ", res2)
+# # #divisão
 
-# #Operador NOT
+# # div = n1 / n2
+# # print("Divisão: ", div)
 
-# res3 = not n1
-# print("NOT: ", res3)
+# # #resto da divisão
 
-#=======================================================================================================================================#
+# # resto = n1 % n2
+# # print("Resto da divisão: ", resto)
 
-# print("===========Operadores Lógicos===========\n")
+# # #exponenciação
 
-# idade = 32
-# altura = 1.75
+# # exp = n1 ** n2
+# # print("Exponenciação: ", exp)
 
-# resultado = (idade >= 18) and (altura >= 1.70)
-# msg = "Pode participar do evento? " + str(resultado)
-# print(msg)
+#     # 1° parenteses ()
+#     # 2° exponenciação
+# #     # 3° multiplicação e divisão
+# #     # 4° soma e subtração
+# #     # calc = 2 * (2*3) + 1
+# # calc = ((2*(2*3)) + 1)
+# # print("Valor do Cálculo: ", calc)
 
-#=======================================================================================================================================#
+# # x = y = z = 0
 
-# print("==========Programa de disparo de alarme==========\n")
+# # x = int (input("Digite um numero: "))
+# # y = int (input("Digite outro numero: "))
 
-# porta = "a"
-# janela = "f"
+# # z = x + y
 
-# alarme = (porta == "f") and (janela == "a")
+# # print("A soma dos valores é ", z)
 
-# mgs = "Alarme disparado? " + str(alarme)
-# print(mgs)
+# # atribuição
 
-#=======================================================================================================================================#
+# n1 = 3
+# n2 = 2
 
-# print ("=Adição, subtração, multiplicação, divisão, resto da divisão e exponenciação=\n")
+# # igualdade relacionais
+# #tem como resposta: true ou false
 
-# n1 = 10
-# n2 = 3
+# #igualdade ==
+# res1 = (n1 == n2)
+# print(res1)
 
-# #adição
-# soma = n1 + n2
-# print("Adição: ", soma)
+# #diferença !=
+# res2 = (n1 != n2)
+# print(res2)
 
-# #subtração
+# #maior que >
+# res3 = (n1 > n2)
+# print(res3)
 
-# sub = n1 - n2
-# print("Subtração: ", sub)
+# #menor que <
+# res4 = (n1 < n2)
+# print(res4)
 
-# #multiplicação
+# #maior ou igual >=
+# res5 = (n1 >= n2)
+# print(res5)
 
-# mult = n1 * n2
-# print("Multiplicação: ", mult)
+# #menor ou igual <=
 
-# #divisão
+# res6 = (n1 <= n2)
+# print(res6)
 
-# div = n1 / n2
-# print("Divisão: ", div)
+x = y = z = False
 
-# #resto da divisão
+print ("Digite um número: ")
+n1 = int(input())
+n2 = int(input("Digite outro número: "))
 
-# resto = n1 % n2
-# print("Resto da divisão: ", resto)
+x = n1 == n2
+print("São iguais? ", x, "\n")
 
-# #exponenciação
+z = n1 > n2
+print(n1, "É maior que ", n2,'?', z, "\n")
 
-# exp = n1 ** n2
-# print("Exponenciação: ", exp)
-
-    # 1° parenteses ()
-    # 2° exponenciação
-#     # 3° multiplicação e divisão
-#     # 4° soma e subtração
-#     # calc = 2 * (2*3) + 1
-# calc = ((2*(2*3)) + 1)
-# print("Valor do Cálculo: ", calc)
-
-# x = y = z = 0
-
-# x = int (input("Digite um numero: "))
-# y = int (input("Digite outro numero: "))
-
-# z = x + y
-
-# print("A soma dos valores é ", z)
-
-# atribuição
-
-n1 = 3
-n2 = 2
-
-# igualdade relacionais
-#tem como resposta: true ou false
-
-#igualdade ==
-res1 = (n1 == n2)
-print(res1)
-
-#diferença !=
-res2 = (n1 != n2)
-print(res2)
-
-#maior que >
-res3 = (n1 > n2)
-print(res3)
-
-#menor que <
-res4 = (n1 < n2)
-print(res4)
-
-#maior ou igual >=
-res5 = (n1 >= n2)
-print(res5)
-
-#menor ou igual <=
-
-res6 = (n1 <= n2)
-print(res6)
+y = n1 != n2
+print("São diferentes? " + str(y))
