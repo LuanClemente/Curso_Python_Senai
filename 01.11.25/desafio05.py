a050b515656d1147c3d74c9cf11bd606811a8002
Luan Clemente Costa <79475114+LuanClemente@users.noreply.github.com>
2025-12-13 12:15:27 +0000

    Adicionando desafio05 e configurações

---
+++ 01.11.25/desafio05.py
@@ -0,0 +1,7 @@
+largura = float (input("Digite a largura do retangulo: "))
+altura = float (input("Digite a altura do retangulo: "))
+
+area = largura * altura
+perimetro = 2 * (largura + altura)
+
+print(f"A área é {area} e o perimetro é {perimetro}.")
